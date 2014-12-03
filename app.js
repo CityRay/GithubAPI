a27e3177a5ab641ec503142e5d31f15fc4d264a9
RayLin <ray102467@gmail.com>
2014-12-03 21:59:58 +0800

    v1

---
+++ app.js
@@ -0,0 +1,48 @@
+(function () {
+  // app.js
+var app = angular.module('githubsearch', []);
+
+app.controller('SearchController', function ($scope, $http)
+{
+
+  this.noUserText="No User Info Found";
+
+  this.getGitInfo = function(){
+      //onsole.log('hi');
+      var foo = this;
+      foo.userNotFound = false;
+      foo.loaded = false;
+
+      $http.get("https://api.github.com/users/" + foo.username)
+          .success(function (data, status) {
+              if (data.name == "") data.name = data.login;
+              foo.user = data;
+              console.log('status: '+status);
+              console.log(foo.user);
+              foo.loaded = true;
+
+           }).error(function (data, status) {
+              foo.userNotFound = true;
+              //console.log(status);
+
+           });
+
+      $http.get("https://api.github.com/users/" + this.username + "/repos")
+          .success(function (data) {
+            foo.repos = data;
+            foo.reposFound = data.length > 0;
+            console.log('repoData: =======================');
+            console.log(data);
+            console.log('=================================');
+          }).error(function(e) {
+
+          });
+
+  };
+
+});
+
+})();
+
+
+
